d12d697f9fd5776a487bb1fca7fc453a419d83b8
talr0704 <103999281+talr0704@users.noreply.github.com>
2022-06-26 01:10:51 +0300

    good

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,37 @@
+﻿var firstSlide = 1;
+showSlides(firstSlide);
+
+
+function plusSlides(n) {
+    showSlides(firstSlide += n);
+}
+
+
+function currentSlide(n) {
+    showSlides(firstSlide = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var mini = document.getElementsByClassName("mini");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {
+        firstSlide = 1
+    }
+    if (n < 1) {
+        firstSlide = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < mini.length; i++) {
+        mini[i].className = mini[i].className.replace(" active", "");
+    }
+    slides[firstSlide - 1].style.display = "block";
+    mini[firstSlide - 1].className += " active";
+    captionText.innerHTML = mini[firstSlide - 1].alt;
+    console.log(mini.length);
+    console.log(slides.length);
+
+}
